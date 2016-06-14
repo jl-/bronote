bae4ce0be77e5363d3a442546eca00d561f7253b
jl- <jlz.cius@gmail.com>
2016-06-14 23:29:44 +0800

    add i18n terms

--- i18n/locales/en/index.js
+++ i18n/locales/en/index.js
@@ -4,6 +4,9 @@ const lang = new Locale();
 
 lang.set('terms', {
   create: 'Create',
+  cancel: 'Cancel',
+  submit: 'Submit',
+  confirm: 'Confirm',
 }).set('app.sections', {
   recent: 'Recent',
   notebooks: 'Notebooks',
@@ -11,12 +14,14 @@ lang.set('terms', {
   phrases: {
     createNotebook: 'Create Notebook',
     newNoteIn: 'New Note in ',
+    notebookName: 'Notebook Name',
   },
 }).set('app.search', {
   phrases: {
     placeholder: 'Serch Notes',
   },
 }).set('errors', {
+  NOTEBOOK_NAME_INVALID: 'notebook name at least 3 characters',
 });
 
 export const kvs = lang.flatten();
