0927e9fce42876c9eb37f8f756c359478773aa7c
jl- <jlz.cius@gmail.com>
2016-06-14 23:33:22 +0800

    delete modules/folders, partials/_editor

--- src/modules/folders/action-types.js
+++
@@ -1,9 +0,0 @@
-import uid from 'utils/lib/uid';
-import ktv from 'utils/lib/ktv';
-
-const ACTIONS = [
-  'PICK_FOLDER',
-  'PICK_FOLDER_OK',
-  'PICK_FOLDER_FAILED'
-];
-export default ktv(ACTIONS, uid);
--- src/modules/folders/reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import folder from './reducers/folder';
-
-const folders = combineReducers({ folder });
-
-export default folders;
